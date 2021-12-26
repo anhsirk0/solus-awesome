b36fe115262b5c2bf2629e252c80e1c10d8af53e
krishna <krishna404@gmail.com>
2021-12-26 17:17:31 +0530

    First commit

---
+++ remove_awesome_depends.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo eopkg remove luarocks \
+                  make \
+                  gobject-introspection-devel \
+                  pkg-config \
+                  g++ \
+                  libcairo-devel \
+                  libx11-devel \
+                  libxcb-devel \
+                  xcb-util-cursor-devel \
+                  xcb-util-devel \
+                  gdk-pixbuf-devel \
+                  xcb-util-keysyms-devel \
+                  xcb-util-wm-devel \
+                  xcb-util-xrm-devel \
+                  libxkbcommon-devel \
+                  libstartup-notification-devel \
+                  libxdg-basedir-devel \
+                  meson \
+                  ninja
